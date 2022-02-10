8efa10f948df9e944f7f2cb4a809c072d49d9fd7
AkashMete <73811249+AkashMete@users.noreply.github.com>
2022-02-10 04:34:09 -0800

    Update connfile.php

--- connfile.php
+++ connfile.php
@@ -2,7 +2,7 @@
 $mysql_hostname = "remotemysql.com";
 $mysql_user = "EeMQv0iRGM";
 $mysql_password = "HK2n9UyJss";
-$mysql_database = "bankingdata";
+$mysql_database = "EeMQv0iRGM";
 $port = 3306;
 $conn =  mysqli_connect($mysql_hostname,$mysql_user,$mysql_password,$mysql_database,$port);
 
